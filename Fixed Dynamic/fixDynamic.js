50169f3a99a2870b9004c118574b296eb39d6016
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-04-28 21:54:43 +0600

    fix dynamic array added

--- Fixed Dynamic/fixDynamic.js
+++ Fixed Dynamic/fixDynamic.js
@@ -1,1 +1,12 @@
-console.log("hi!!");
+const size = parseInt(prompt("Enter size of array : "));
+
+const myArr = new Array(size);
+let i;
+
+for (i = 0; i < size; i++) {
+  myArr[i] = i * 2;
+}
+
+for (i = 0; i < size; i++) {
+  console.log(myArr);
+}
